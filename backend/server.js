e0870ef889fb1cc08453b166976638505fb1bd8a
Mtaaha94 <muhammadtaaha.94@gmail.com>
2021-07-13 13:04:21 +0500

    somechanges

--- backend/server.js
+++ backend/server.js
@@ -33,9 +33,17 @@ app.get("/api/config/paypal", (req, res) =>
 
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
-app.get("/", (req, res) => {
-  res.send("API is running....");
-});
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/client/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running....");
+  });
+}
 
 app.use(notFound);
 app.use(errorHandler);
